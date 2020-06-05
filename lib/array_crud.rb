421139566c3aded61a423de57f74d30437440df9
Corey <coreyjmartin2@gmail.com>
2020-06-05 01:08:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,6 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   elements = ["wow", "earth", "am", "water", "fire", "arrays!"]
+  puts "#{elements[2]}"
 end
 
 def retrieve_first_element_from_array(array)
