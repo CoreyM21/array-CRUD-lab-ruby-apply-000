f9fd7272ddea657deedaa94f3d77d5eed158f7d5
Corey <coreyjmartin2@gmail.com>
2020-06-05 01:03:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  elements = ["earth", "wind", "water", "fire", "array"]
-  array_elements = elements.pop
+  elements = ["earth", "wind", "water", "fire", "arrays!"]
+  arrays_elements = elements.pop
 end
 
 def remove_element_from_start_of_array(array)
