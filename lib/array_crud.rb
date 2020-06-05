01c68f64321cb97e7c29de49384fbac3c3316c0f
Corey <coreyjmartin2@gmail.com>
2020-06-05 01:15:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,26 +3,26 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["earth", "wind", "water", "fire"]
+  ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
-  elements = ["earth", "wind", "water", "fire"]
+  elements = ["I", "am", "really", "learning"]
   elements << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  elements = ["earth", "wind", "water", "fire"]
+  elements = ["I", "am", "really", "learning", "arrays!"]
   elements .unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  elements = ["earth", "wind", "water", "fire", "arrays!"]
+  elements = ["wow", "I", "am", "really", "learning", "arrays!"]
   arrays_elements = elements.pop
 end
 
 def remove_element_from_start_of_array(array)
-  elements = ["wow", "earth", "wind", "water", "fire", "arrays!"]
+  elements = ["wow", "I", "am", "really", "learning", "arrays!"]
   wow_elements = elements.shift 
 end
 
@@ -32,9 +32,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
+  ["wow", "I", "am", "really", "learning", "arrays!"]
   
 end
 
 def retrieve_last_element_from_array(array)
+  ["wow", "I", "am", "really", "learning", "arrays!"]
   
 end
