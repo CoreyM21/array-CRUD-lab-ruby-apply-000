b2d657d906e5cc4e513629262de635405ec7dade
Corey <coreyjmartin2@gmail.com>
2020-06-05 00:53:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   elements = ["earth", "wind", "water", "fire"]
-  elements.push("element")
+  elements << element
 end
 
 def add_element_to_start_of_array(array, element)
