49dfc26e918d83b8669dae945411378a6177674b
Corey <coreyjmartin2@gmail.com>
2020-06-05 01:20:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,10 +33,10 @@ end
 
 def retrieve_first_element_from_array(array)
   ["wow", "I", "am", "really", "learning", "arrays!"]
-  
+  print "#{array[0]}"
 end
 
 def retrieve_last_element_from_array(array)
   ["wow", "I", "am", "really", "learning", "arrays!"]
-  
+  print "#{array[5]}"
 end
